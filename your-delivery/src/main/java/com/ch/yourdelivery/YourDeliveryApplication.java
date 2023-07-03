04a97adc9af641f9779cab218fd4790f8cecc07d
chyn00 <ybj0749@gmail.com>
2023-07-03 22:14:41 +0900

    spring boot init

---
+++ your-delivery/src/main/java/com/ch/yourdelivery/YourDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.ch.yourdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourDeliveryApplication.class, args);
+	}
+
+}
---
+++ your-delivery/src/test/java/com/ch/yourdelivery/YourDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.yourdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4b087614ff2e8513abf8cd049c51ef8757be6c05
신충현 <ybj0749@gmail.com>
2023-07-28 23:14:02 +0900

    add. 사장님 회원가입

---
+++ your-delivery/src/main/java/com/ch/yourdelivery/owner/respository/OwnerRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.ch.yourdelivery.owner.respository;
+
+import com.ch.yourdelivery.owner.domain.model.Owner;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+
+@Repository
+public class OwnerRepositoryImpl implements OwnerRepository {
+
+    final HashMap<String, Owner> ownerTable = new HashMap<String, Owner>();
+    static long size = 0;
+
+    @Override
+    public Owner save(Owner owner) {
+
+        String email = owner.getEmail();
+        owner.setId((Long.valueOf(++size)));
+        ownerTable.put(email, owner);
+        return owner;
+    }
+
+    @Override
+    public Owner findByEmail(String email) {
+
+        if (ownerTable.containsKey(email)) {
+            return ownerTable.get(email);
+        }
+
+        return new Owner();
+    }
+}
---
+++ your-delivery/src/main/java/com/ch/yourdelivery/owner/service/OwnerService.java
@@ -0,0 +1,22 @@
+package com.ch.yourdelivery.owner.service;
+
+import com.ch.yourdelivery.owner.domain.model.Owner;
+import com.ch.yourdelivery.owner.respository.OwnerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public Owner emailSignUp(Owner owner) {
+
+        if (ownerRepository.findByEmail(owner.getEmail()).getEmail() == null) {
+            return ownerRepository.save(owner);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}

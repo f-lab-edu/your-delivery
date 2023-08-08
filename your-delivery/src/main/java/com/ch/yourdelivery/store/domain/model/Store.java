f5a41bc5e04f953f6901e3f8900645388099d807
chyn00 <ybj0749@gmail.com>
2023-08-09 08:00:23 +0900

    fix. 같은 이름의 동네이름을 구분하기 위해, 동 이름을 객체로 관리 지번과, 도로명 주소를 함께 저장

--- your-delivery/src/main/java/com/ch/yourdelivery/store/domain/model/Store.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/store/domain/model/Store.java
@@ -13,7 +13,7 @@ public class Store {
     private String name;
     private List<OperatingTimeInMonth> operatingTimeList; //운영시간
     private String phoneNumber;
-    private List<String> deliveryLocation; //배달가능지역
+    private List<DeliveryLocation> deliveryLocation; //배달가능지역
     private String descriptionForNotification; //이벤트나, 공지사항등의 설명란
     private StoreLocation storeLocation; //가게 위치
 

46dd1b3689bb1803b76f07025d114d349f7aad60
신충현 <ybj0749@gmail.com>
2023-08-13 20:20:33 +0900

    update. StoreResponse String -> DeliveryLocation

--- your-delivery/src/main/java/com/ch/yourdelivery/store/domain/dto/StoreResponse.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/store/domain/dto/StoreResponse.java
@@ -1,5 +1,6 @@
 package com.ch.yourdelivery.store.domain.dto;
 
+import com.ch.yourdelivery.store.domain.model.DeliveryLocation;
 import com.ch.yourdelivery.store.domain.model.OperatingTimeInMonth;
 import com.ch.yourdelivery.store.domain.model.StoreLocation;
 import lombok.Builder;
@@ -15,7 +16,7 @@ public class StoreResponse {
     private String name;
     private List<OperatingTimeInMonth> operatingTimeList; //운영시간
     private String phoneNumber;
-    private List<String> deliveryLocation; //배달가능지역
+    private List<DeliveryLocation> deliveryLocation; //배달가능지역
     private String descriptionForNotification; //이벤트나, 공지사항등의 설명란
     private StoreLocation storeLocation; //가게 위치
 

70dbcfca8116a7e97ae2bc4ebf71e8c2b8864eae
신충현 <ybj0749@gmail.com>
2023-08-17 21:00:18 +0900

    add. Exception code 공통 처리를 위해 HttpStatus 필드 추가 Handler에 변경사항이 없도록, HttpStatus 필드로 관리

--- your-delivery/src/main/java/com/ch/yourdelivery/common/exception/ExceptionCode.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/common/exception/ExceptionCode.java
@@ -2,11 +2,13 @@ package com.ch.yourdelivery.common.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ExceptionCode {
-    CONFLICT("CONFLICT");
+    CONFLICT("CONFLICT", HttpStatus.CONFLICT);
 
     String message;
+    HttpStatus httpStatus;
 }

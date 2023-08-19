532e7d252fcf3b7717933ac50aa51e71a02cd8c3
신충현 <ybj0749@gmail.com>
2023-08-19 16:54:37 +0900

    update. 공통 response 생성자 lombok으로 변경

--- your-delivery/src/main/java/com/ch/yourdelivery/common/response/DeliveryResponse.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/common/response/DeliveryResponse.java
@@ -1,20 +1,15 @@
 package com.ch.yourdelivery.common.response;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+@AllArgsConstructor
 public class DeliveryResponse {
 
     String message;
     Object data;
     HttpStatus httpStatus;
-
-    public DeliveryResponse(String message, Object data, HttpStatus httpStatus) {
-        this.message = message;
-        this.data = data;
-        this.httpStatus = httpStatus;
-    }
-
-    public DeliveryResponse(String message, HttpStatus httpStatus) {
+    public DeliveryResponse(String message, HttpStatus httpStatus) {//추가 생성자
         this.message = message;
         this.httpStatus = httpStatus;
     }

318f3006f2fc038e008992ad53b73184e2459323
lion <lion@f-lab.kr>
2023-08-24 20:14:33 +0900

    login 기능 리팩토링

--- your-delivery/src/main/java/com/ch/yourdelivery/common/response/DeliveryResponse.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/common/response/DeliveryResponse.java
@@ -1,18 +1,16 @@
 package com.ch.yourdelivery.common.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
-@AllArgsConstructor
-@Getter
-public class DeliveryResponse {
 
-    String message;
-    Object data;
-    HttpStatus httpStatus;
-    public DeliveryResponse(String message, HttpStatus httpStatus) {//추가 생성자
-        this.message = message;
-        this.httpStatus = httpStatus;
+public class DeliveryResponse<T> {
+    T data;
+
+    public DeliveryResponse(T data) {
+        this.data = data;
+    }
+
+    public static <T> DeliveryResponse<T> of(T data) {
+        return new DeliveryResponse<>(data);
     }
 }

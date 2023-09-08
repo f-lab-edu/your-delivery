62a66d3da95b3af9ec405b1f1fc2dbceca51f403
신충현 <ybj0749@gmail.com>
2023-09-09 08:42:01 +0900

    dalete. 불필요한 entity 제거

--- your-delivery/src/main/java/com/ch/yourdelivery/store/controller/StoreController.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/store/controller/StoreController.java
@@ -1,14 +1,10 @@
 package com.ch.yourdelivery.store.controller;
 
-import com.ch.yourdelivery.store.domain.dto.StoreRequest;
 import com.ch.yourdelivery.store.domain.dto.StoreResponse;
-import com.ch.yourdelivery.store.domain.model.Store;
 import com.ch.yourdelivery.store.service.StoreService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,31 +14,9 @@ public class StoreController {
 
     private final StoreService storeService;
 
-    //가게정보 단건저장
-    @PostMapping("/store")
-    public StoreResponse createStore(
-        @RequestBody StoreRequest storeRequest) {
-
-        Store store =
-            Store.builder().name(storeRequest.getName()).ownerId(storeRequest.getOwnerId())
-                .operatingTimeList(storeRequest.getOperatingTimeList())
-                .phoneNumber(storeRequest.getPhoneNumber())
-                .deliveryLocation(storeRequest.getDeliveryLocation())
-                .descriptionForNotification(storeRequest.getDescriptionForNotification())
-                .storeLocationXY(storeRequest.getStoreLocationXY()).build();
-
-        store = storeService.saveStore(store);
-
-        return StoreResponse.builder().id(store.getId()).ownerId(store.getOwnerId())
-            .name(store.getName()).operatingTimeList(store.getOperatingTimeList())
-            .phoneNumber(store.getPhoneNumber()).deliveryLocation(store.getDeliveryLocation())
-            .descriptionForNotification(store.getDescriptionForNotification())
-            .storeLocationXY(store.getStoreLocationXY()).build();
-    }
-
     //가게정보 다건조회
     @GetMapping("/stores")
-    public List<Store> findStores(@RequestParam("ownerId") String ownerId) {
+    public List<StoreResponse> findStores(@RequestParam("ownerId") String ownerId) {
         return storeService.findAllByOwnerId(ownerId);
     }
 
--- your-delivery/src/main/java/com/ch/yourdelivery/store/service/StoreService.java
+++ your-delivery/src/main/java/com/ch/yourdelivery/store/service/StoreService.java
@@ -1,22 +1,16 @@
 package com.ch.yourdelivery.store.service;
 
-import com.ch.yourdelivery.store.domain.model.Store;
-import com.ch.yourdelivery.store.repository.StoreRepository;
+import com.ch.yourdelivery.store.domain.dto.StoreResponse;
+import java.util.Collections;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
 public class StoreService {
 
-    private final StoreRepository storeRepository;
-
-    public Store saveStore(Store store) {
-        return storeRepository.save(store);
-    }
-
-    public List<Store> findAllByOwnerId(String ownerId) {
-        return storeRepository.findAllByOwnerId(ownerId);
+    // StoreResponse list 를 반환해주는 함수 작성예정
+    // 다른 Interface 에서 가져온 데이터로 가정(ex. API, Interface)
+    public List<StoreResponse> findAllByOwnerId(String ownerId) {
+        return Collections.EMPTY_LIST;
     }
 }
